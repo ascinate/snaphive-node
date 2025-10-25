f7f91ac36aa5978b4bf46fc1158d2086682a6518
abu-saied-mondal <abumondal990@gmail.com>
2025-10-25 18:12:02 +0530

    profile update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -203,8 +203,26 @@ const forgotPassword = async (req, res) => {
     res.status(500).json({ message: "Failed to resend OTP" });
   }
 };
+const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id; // from protect middleware (token)
+    const { name, email } = req.body;
 
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
 
+    if (name) user.name = name;
+    if (email) user.email = email;
 
+    await user.save();
 
-module.exports = { register, login, verifyOTP, forgotPassword, resetPassword, resendOTP};
+    res.json({
+      message: "Profile updated successfully",
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+module.exports = { register, login, verifyOTP, forgotPassword, resetPassword, resendOTP, updateProfile };
+
