ec22e3dd2ac120546ccf92d1be0770c73435894e
ski12345491 <abu@ascinatetech.com>
2025-10-30 13:20:57 +0530

    new

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const path = require("path");
+
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 
 connectDB();
 
