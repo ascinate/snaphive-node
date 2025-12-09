acf108b0b980c35524bd872aeaa2e4524ace45fe
unknown <sourodip@ascinatetech.com>
2025-12-09 17:30:44 +0530

    fix

--- src/routes/hiveRoutes.js
+++ src/routes/hiveRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const protect = require("../middleware/authMiddleware");
-const { createHive, getUserHives, uploadHiveImages } = require("../controllers/hiveController");
+const { createHive, getUserHives, uploadHiveImages, inviteMemberByEmail, acceptHiveInvite } = require("../controllers/hiveController");
 
 // Use memory storage for Vercel + Firebase
 const upload = multer({
