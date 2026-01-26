ac63c5ba06ed2301e7f52bee253913fe95dc6a3b
ski12345491 <abu@ascinatetech.com>
2026-01-26 14:41:21 +0530

    updated

--- src/routes/adminUserRoutes.js
+++ src/routes/adminUserRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getAllUsers } = require("../controllers/adminController");
+const { getAllUsers,getUserProfileAdmin,toggleUser,softDeleteUser,resetUserAccount } = require("../controllers/adminController");
 const adminAuth = require("../middleware/adminAuth");
 
 const router = express.Router();
 
 router.get("/", adminAuth,getAllUsers);
+router.get("/profile/:id", getUserProfileAdmin);
+router.get("/toggle/:id", toggleUser);
+router.get("/delete/:id", softDeleteUser);
+router.get("/reset/:id", resetUserAccount);
+
 
 module.exports = router;
