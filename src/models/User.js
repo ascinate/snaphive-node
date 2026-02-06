1a5a4cb320bcff85cf115f1991f0cf4ebcfdca63
Pritam Ghosh <pritam7599@gmail.com>
2026-02-06 12:42:51 +0530

    fcm token

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-
+     fcmToken: { type: String, default: null },
     otp: { type: String },
     otpExpires: { type: Date },
     isVerified: { type: Boolean, default: false },
