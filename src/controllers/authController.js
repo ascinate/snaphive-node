7ffa24a056e5b148e67d07fa48a959e349a9926b
ski12345491 <abu@ascinatetech.com>
2026-02-12 15:31:42 +0530

    profile upload

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-const bucket = require("../config/firebase");
+const { bucket } = require("../config/firebase");
 const fs = require("fs");
 const appleSigninAuth = require("apple-signin-auth");
 const generateOTP = () => Math.floor(1000 + Math.random() * 9000).toString();
