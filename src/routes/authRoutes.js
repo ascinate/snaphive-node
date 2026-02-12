3d5ba6de785603b4b5c6ff9f7b2b43a0413d67e6
ski12345491 <abu@ascinatetech.com>
2026-02-12 15:50:39 +0530

    profile upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -16,29 +16,7 @@ const fs = require("fs");
 
 const router = express.Router();
 
-const uploadDir =
-  process.env.VERCEL || process.env.NODE_ENV === "production"
-    ? "/tmp/uploads"
-    : "uploads";
 
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "_" + file.originalname);
-  },
-});
-
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) cb(null, true);
-  else cb(new Error("Only image files are allowed"), false);
-};
-
-const upload = multer({ storage, fileFilter });
 
 router.post("/register", register);
 router.post("/verify-otp", verifyOTP);
@@ -46,7 +24,7 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/resend-otp", resendOTP);
-router.put("/update-profile", protect, upload.single("profileImage"), updateProfile);
+router.put("/update-profile", protect, updateProfile);
 
 router.get("/profile", protect, (req, res) => {
   res.json({ message: "Access granted", user: req.user });
