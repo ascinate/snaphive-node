af1762620c8601d4124299059b574af134d55726
ski12345491 <abu@ascinatetech.com>
2026-06-01 15:32:12 +0530

    new user delete

--- src/controllers/stockImageController.js
+++ src/controllers/stockImageController.js
@@ -16,7 +16,7 @@ const getAllImages = async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Server error");
+    res.status(500).send("Server eror");
   }
 };
 
